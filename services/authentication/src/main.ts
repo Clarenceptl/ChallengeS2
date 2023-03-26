8cdd3f63ed1a87b747d5d0ebe66e8c42b23630a9
Clarence Potel <potel.clarence@gmail.com>
2023-03-26 15:21:27 +0200

    conteneurisation des services

--- services/authentication/src/main.ts
+++ services/authentication/src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.AUTH_PORTS || 3023);
+  await app.listen(3022);
 }
 bootstrap();
