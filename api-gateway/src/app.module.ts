f5a45cb5e7f61849a777d8f3308721b4f3b86141
Clarence Potel <potel.clarence@gmail.com>
2023-04-15 19:19:24 +0200

    add microservice

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    // ClientsModule.register([
+    //   {
+    //     name: 'APPLICATION_SERVICE',
+    //     transport: Transport.TCP
+    //   }
+    // ])
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
