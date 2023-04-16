ed23c0ca3e1d12b52b8c4b6ecf591b99175538af
Locman Diawadoh <locman@locman.fr>
2023-04-16 19:16:17 +0200

    vuetify confing done, color added as an example

---
+++ app/src/plugins/vuetify.js
@@ -0,0 +1,64 @@
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// export const THEME_COLORS = {
+//   'yellow-800': '#FFC107',
+//   'yellow-700': '#FFCA28',
+//   'yellow-600': '#FFD54F',
+//   'yellow-500': '#FFE082',
+//   'yellow-400': '#FFECB3',
+//   'yellow-300': '#FFF8E1',
+//   'yellow-200': '#FFFDE7',
+//   'yellow-100': '#FFFFF8',
+//   'red-800': '#F44336',
+//   'red-700': '#EF5350',
+//   'red-600': '#E57373',
+//   'red-500': '#EF9A9A',
+//   'red-400': '#FFCDD2',
+//   'red-300': '#FFEBEE',
+//   'red-200': '#FFCDD2',
+//   'red-100': '#FFEBEE',
+//   'blue-800': '#2196F3',
+//   'blue-700': '#42A5F5',
+//   'blue-600': '#64B5F6',
+//   'blue-500': '#90CAF9',
+//   'blue-400': '#BBDEFB',
+//   'blue-300': '#E3F2FD',
+//   'blue-200': '#E3F2FD',
+//   'blue-100': '#E3F2FD',
+//   'green-800': '#4CAF50',
+//   'green-700': '#66BB6A',
+//   'green-600': '#81C784',
+//   'green-500': '#A5D6A7',
+//   'green-400': '#C8E6C9',
+//   'green-300': '#E8F5E9',
+//   'green-200': '#E8F5E9',
+//   'green-100': '#E8F5E9',
+//   'grey-800': '#424242',
+//   'grey-700': '#616161',
+//   'grey-600': '#757575',
+//   'grey-500': '#9E9E9E',
+//   'grey-400': '#BDBDBD',
+//   'grey-300': '#E0E0E0',
+//   'grey-200': '#EEEEEE',
+//   'grey-100': '#F5F5F5',
+// }
+
+export default createVuetify({
+  components,
+  directives,
+  // theme: {
+  //   dark: false,
+  //   themes: {
+  //     light: {
+  //       colors: THEME_COLORS,
+  //     },
+  //     dark: {
+  //       colors: THEME_COLORS,
+  //     },
+  //   },
+  // },
+})
