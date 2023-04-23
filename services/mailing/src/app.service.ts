4a130e631dcfba5a71fd122105b1159ab3526af4
Clarence Potel <potel.clarence@gmail.com>
2023-04-23 02:18:20 +0200

    setup sending email works

--- services/mailing/src/app.service.ts
+++ services/mailing/src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Welcome to mailing service !';
+  constructor(private readonly mailerService: MailerService) {}
+
+  public example(): void {
+    this.mailerService
+      .sendMail({
+        to: 'test@test.com',
+        from: 'noreply@nestjs.com',
+        subject: 'Testing Nest Mailermodule with template ✔',
+        template: 'test', // The `.hbs` extension is appended automatically.
+        context: {
+          // Data to be sent to template engine.
+          code: 'cf1a3f828287',
+          username: 'john doe'
+        }
+      })
+      .then(() => console.log('Email sent'))
+      .catch((e) => console.log('Error sending email', e));
   }
 }
