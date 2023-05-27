8d6c86137fdb75ddd0fc2183e2e0ff338cf1f9e5
Clarence Potel <potel.clarence@gmail.com>
2023-05-27 22:06:52 +0200

    disable swagger prod + handle cors

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -4,15 +4,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    .setTitle('Api gateway')
-    .setDescription("Les requêtes de l'api gateway")
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
+  // const corsOptions = {
+  //   origin: 'http://localhost:3000',
+  //   credentials: true
+  // };
+  app.enableCors();
+  if (process.env.NODE_ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('Api gateway')
+      .setDescription("Les requêtes de l'api gateway")
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
   await app.listen(3080);
 }
 bootstrap();
