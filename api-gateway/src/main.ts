ebb3a6f7adc08e12a38beb0c63f7779eb547ced5
Clacla <64362553+Clarenceptl@users.noreply.github.com>
2023-06-04 16:32:37 +0200

    Feature/versionning_compression (#32)
    * add versionning and responses compression
    
    * add versionning and compression
    
    * remove test
    
    * clean import
    
    ---------
    
    Co-authored-by: SoulDevt <souleymane.gueye.pro@gmail.com>

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +10,10 @@ async function bootstrap() {
   //   origin: 'http://localhost:3000',
   //   credentials: true
   // };
+  app.use(compression());
+  app.enableVersioning({
+    type: VersioningType.URI
+  });
   app.enableCors();
   if (process.env.NODE_ENV === 'development') {
     const config = new DocumentBuilder()
