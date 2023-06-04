80855a65639aa4a44184e41369c5324eff09bdfa
Clacla <64362553+Clarenceptl@users.noreply.github.com>
2023-06-04 17:00:31 +0200

    Feature/heardersecurity (#33)
    * add helmet for headers security
    
    * update package
    
    ---------
    
    Co-authored-by: SoulDevt <souleymane.gueye.pro@gmail.com>

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 import { VersioningType } from '@nestjs/common';
 import * as compression from 'compression';
 
@@ -10,6 +11,7 @@ async function bootstrap() {
   //   origin: 'http://localhost:3000',
   //   credentials: true
   // };
+  app.use(helmet());
   app.use(compression());
   app.enableVersioning({
     type: VersioningType.URI
