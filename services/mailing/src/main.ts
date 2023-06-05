333d4fcbcfd4d7161dc14ea3b8ce41c165995f07
Clarence Potel <potel.clarence@gmail.com>
2023-06-05 13:04:11 +0200

    add package mail

--- services/mailing/src/main.ts
+++ services/mailing/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport } from '@nestjs/microservices';
+import * as compression from 'compression';
+import helmet from 'helmet';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-// TODO: voir si on laisse accessible à l'extérieur
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.connectMicroservice({
@@ -12,6 +14,19 @@ async function bootstrap() {
       port: 3024
     }
   });
+
+  app.use(helmet());
+  app.use(compression());
+  app.enableCors();
+  if (process.env.NODE_ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('Api Mailing')
+      .setDescription("Les requêtes de l'api mailing")
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
   await app.startAllMicroservices();
   await app.listen(3023);
 }
