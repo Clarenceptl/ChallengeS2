04e33f2cfc52aed1a68d4e089b5b0d90559f359e
boukhatem <mboukhatem777@gmail.com>
2023-06-12 20:36:13 +0200

    Admin list users

--- app/src/views/admin/UsersList.vue
+++ app/src/views/admin/UsersList.vue
@@ -1,67 +1,65 @@
 <template>
   <div>
-    <h1 class="text-center mb-4">Users List</h1>
-    <div class="scrollable">
-      <v-row class="pa-5">
-        <v-col cols="6" class="text-center" v-for="user in users" :key="user.id">
-          <div class="d-flex justify-center mb-2">
-            <v-card class="w-100 pa-2 bg-yellow-100" variant="outlined" elevation="4">
-              <v-card-title>
-                <h2>{{ user.firstname }} {{ user.lastname }}</h2>
-              </v-card-title>
-              <v-card-subtitle>
-                <h3>{{ user.email }}</h3>
-              </v-card-subtitle>
-              <v-card-text>
-                <h4>{{ user.birthdate }}</h4>
-              </v-card-text>
-              <div>
-                <v-btn class="w-25" color="blue-500" @click="showDialog = true; selectedUser = user">Edit</v-btn>
-                <v-btn class="w-25 ml-2" color="red-500">Delete</v-btn>
-              </div>
+    <h1 class="text-center my-10">Users List</h1>
+    <v-row class="pa-5">
+      <v-col cols="12" class="text-center">
+        <div class="d-flex justify-center mb-2">
+          <div class="container">
+            <h3 class="my-10 text-center">The current page displays the user list, providing options to delete or modify
+              users.</h3>
+            <v-card class="mx-auto">
+              <v-toolbar color="appgrey">
+                <v-toolbar-title></v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+
+              <v-simple-table dense>
+                <template v-slot:default>
+                  <thead>
+                    <tr>
+                      <th class="py-4 px-4">Firstname</th>
+                      <th class=" px-4">Lastname</th>
+                      <th class=" px-4">Email</th>
+                      <th class=" px-4">Birthdate</th>
+                      <th class=" px-4">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="user in users" :key="user.id">
+                      <td class="py-4 px-4">{{ user.firstname }}</td>
+                      <td class="px-4">{{ user.lastname }}</td>
+                      <td class="px-4">{{ user.email }}</td>
+                      <td class="px-4">{{ user.birthdate }}</td>
+                      <td><v-icon color="blue" @click="showDialog = true; selectedUser = user">mdi-pencil</v-icon></td>
+                      <td><v-icon color="red">mdi-delete</v-icon></td>
+                    </tr>
+                  </tbody>
+                </template>
+              </v-simple-table>
             </v-card>
           </div>
-        </v-col>
-      </v-row>
-    </div>
-    <v-dialog v-model="showDialog" max-width="600">
-      <v-card class="pa-5 bg-green-300" variant="outlined">
-        <v-card-title>
-          <h2>Edit user {{ selectedUser.firstname }} {{ selectedUser.lastname }}</h2>
-        </v-card-title>
-        <v-card-text>
-          <v-text-field
-            clearable
-            v-model="selectedUser.firstname"
-            type="text"
-            color="appgrey"
-            variant="outlined"
-            required
-          />
-          <v-text-field
-            clearable
-            v-model="selectedUser.lastname"
-            type="text"
-            color="appgrey"
-            variant="outlined"
-            required
-          />
-          <v-text-field
-            clearable
-            v-model="selectedUser.birthdate"
-            type="date"
-            color="appgrey"
-            variant="outlined"
-            required
-          />
-        </v-card-text>
-        <v-card-actions>
-          <v-btn color="red-500" text @click="showDialog = false; selectedUser = null">Cancel</v-btn>
-          <v-btn color="blue-800" text>Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+        </div>
+      </v-col>
+    </v-row>
   </div>
+  <v-dialog v-model="showDialog" max-width="600">
+    <v-card class="pa-5 bg-green-300" variant="outlined">
+      <v-card-title>
+        <h2>Edit user {{ selectedUser.firstname }} {{ selectedUser.lastname }}</h2>
+      </v-card-title>
+      <v-card-text>
+        <v-text-field clearable v-model="selectedUser.firstname" type="text" color="appgrey" variant="outlined"
+          required />
+        <v-text-field clearable v-model="selectedUser.lastname" type="text" color="appgrey" variant="outlined" required />
+        <v-text-field clearable v-model="selectedUser.birthdate" type="date" color="appgrey" variant="outlined"
+          required />
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="red-500" text @click="showDialog = false; selectedUser = null">Cancel</v-btn>
+        <v-btn color="blue-800" text>Save</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script setup>
