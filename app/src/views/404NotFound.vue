6c0dfb068c5cb5e1383dfae81c8a2ff2586c493e
boukhatem <mboukhatem777@gmail.com>
2023-06-12 21:59:32 +0200

    404 style

--- app/src/views/404NotFound.vue
+++ app/src/views/404NotFound.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="d-flex flex-column align-center text-centerpa-5">
-      <v-img :src="NotFound" width="100%" height="75vh"/>
+      <v-img :src="NotFound" width="20%" height="75vh"/>
     <h1>Oops, This Page Could Not Be Found</h1>
     <p>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
   </div>
