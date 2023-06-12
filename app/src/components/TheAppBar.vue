abaf08872d3f0dc07caac3ad2f0dffd89f994920
boukhatem <mboukhatem777@gmail.com>
2023-06-12 18:45:40 +0200

    Admin dashboard and navbar fix front

--- app/src/components/TheAppBar.vue
+++ app/src/components/TheAppBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-app-bar :elevation="0" color="white-100">
+    <v-app-bar :elevation="3" color="white-100" >
       <template v-slot:append>
         <v-btn variant="text" color="green-800" @click="router.push('/login')">Login</v-btn>
         <v-btn class="ml-1" variant="text" @click="router.push('/register')">Register</v-btn>
--- app/src/main.js
+++ app/src/main.js
@@ -3,6 +3,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import '@mdi/font/css/materialdesignicons.css';
+
 
 const app = createApp(App)
 
