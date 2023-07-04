c672d59cf3cb7c61acdbc33370bb8792aa523e07
boukhatem <mboukhatem777@gmail.com>
2023-07-04 15:40:19 +0200

    Home fix responsive

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,13 +1,9 @@
 <template>
   <div class="div-background">
     <v-row>
-      <v-col cols="5">
+      <v-col cols="12" sm="5">
         <div class="catch-phrase">Find a job that matches your passion</div>
-        <!-- <div class="small-text mb-16">
-          Hand-picked opportunities to work from home, remotely, freelance, full-time, part-time,
-          contract and internships.
-        </div> -->
-        <div class="smaller-text mt-16">
+        <div class="smaller-text mt-4">
           Supercharge your career with our job search website! Connect with top companies and access
           exclusive opportunities tailored just for you. From entry-level to executive positions,
           our platform offers personalized job recommendations and expert resources. Join our
@@ -17,6 +13,48 @@
     </v-row>
   </div>
 </template>
+
+<style scoped>
+.div-background {
+  background-image: url('@/assets/landing-background.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  height: 93.4vh;
+  padding: 90px; /* Ajout d'un padding pour les grands écrans */
+}
+
+.catch-phrase {
+  font-size: 4rem;
+  font-weight: 700;
+}
+
+.smaller-text {
+  font-size: 1rem;
+  font-weight: 400;
+}
+
+@media (max-width: 768px) {
+  .div-background {
+    background-size: contain;
+    height: 30vh;
+    padding: 20px; /* Utilisation d'un padding plus petit pour les petits écrans */
+  }
+
+  .catch-phrase {
+    font-size: 1rem;
+    text-align: left;
+    
+  }
+
+  .smaller-text {
+    text-align: left;
+    font-size: 0.6rem;
+    max-width: 250px;
+  }
+}
+</style>
 
 <script setup>
 import { onMounted } from 'vue'
@@ -29,49 +67,3 @@ onMounted(async () => {
   await userStore.getUsers()
 })
 </script>
-
-<style scoped>
-.div-background {
-  background-image: url('@/assets/landing-background.png');
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-  width: 100%;
-  height: 93.4vh;
-}
-
-@media (min-width: 768px) {
-  .div-background {
-    padding-top: 90px;
-    padding-left: 90px;
-  }
-  .catch-phrase {
-    font-size: 4rem;
-    font-weight: 700;
-  }
-  .small-text {
-    font-size: 1.5rem;
-    font-weight: 400;
-  }
-  .smaller-text {
-    font-size: 1rem;
-    font-weight: 400;
-  }
-}
-
-@media (max-width: 768px) {
-  .div-background {
-    background-size: contain;
-    height: 30vh;
-    padding-left: 20px;
-  }
-  .catch-phrase {
-    font-size: 1rem;
-    font-weight: 700;
-  }
-  .small-text {
-    font-size: 0.5rem;
-    font-weight: 400;
-  }
-}
-</style>
