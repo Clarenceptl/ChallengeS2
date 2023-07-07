09c4e3612a2fe751b8f695e9690f48e1a868b8c9
Clacla <potel.clarence@gmail.com>
2023-07-07 21:16:22 +0200

    add log for all service

--- services/quiz/src/main.ts
+++ services/quiz/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ExceptionFilter } from './rcp-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
@@ -13,6 +14,7 @@ async function bootstrap() {
       }
     }
   );
+  app.useGlobalFilters(new ExceptionFilter());
   await app.listen();
 }
 bootstrap();
