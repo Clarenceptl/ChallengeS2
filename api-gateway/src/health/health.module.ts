6c1976a8e617245d4bb2f4224b64d29433041278
Clacla <64362553+Clarenceptl@users.noreply.github.com>
2023-08-16 21:49:33 +0200

    Feature/health check (#75)
    * begin health check
    
    * npm install
    
    * add health  check
    
    * clean code

---
+++ api-gateway/src/health/health.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { HealthController } from './health.controller';
+import { TerminusModule } from '@nestjs/terminus';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    TerminusModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_POSTGRES_HOST,
+      port: 5432,
+      username: process.env.DATABASE_POSTGRES_USER,
+      password: process.env.DATABASE_POSTGRES_PASSWORD,
+      database: process.env.DATABASE_POSTGRES_NAME,
+      synchronize: false,
+      autoLoadEntities: true
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_MONGO_URL)
+  ],
+  controllers: [HealthController]
+})
+export class HealthModule {}
