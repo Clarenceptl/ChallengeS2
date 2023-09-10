61728bae0cada4827a3f4679949ca371c6e07af9
Pierre <victsing@outlook.fr>
2023-09-10 22:54:41 +0200

    update elem for pwa

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,18 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import {fileURLToPath, URL} from 'node:url'
+import {VitePWA} from 'vite-plugin-pwa'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 5173
-  },
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    server: {
+        host: '0.0.0.0',
+        port: 5173
+    },
+    plugins: [vue(),
+        VitePWA({
+                registerType: 'autoUpdate'
+            }
+        )
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
